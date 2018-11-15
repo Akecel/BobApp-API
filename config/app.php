6b784cd72c90426ce7e3accaa5b7c2793ed81919
Axel Rayer <axel.rayer@gmail.com>
2018-11-15 15:19:38 +0100

    add Laravel Collective

--- config/app.php
+++ config/app.php
@@ -173,6 +173,14 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /*
+         * Laravel collective
+         */
+
+
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -221,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
