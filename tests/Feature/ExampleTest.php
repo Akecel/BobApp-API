6d9b2c05c07de60461a93292683a381a328f6ac1
Axel Rayer <axel.rayer@gmail.com>
2019-03-17 19:11:20 +0100

    change origin test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        header("Origin: http://104.248.229.222");
         $response = $this->get('/');
 
         $response->assertStatus(200);
