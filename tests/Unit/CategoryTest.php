38d83edd62f7f9c658c268f3192ef87df2117cb7
Axel Rayer <axel.rayer@gmail.com>
2019-03-18 17:06:35 +0100

    change route and model test category

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\FolderCategory;
 
 class CategoryTest extends TestCase
 {
@@ -15,10 +16,10 @@ class CategoryTest extends TestCase
      */
     public function test_can_show_categroy()
     {
-        $categories = App\Models\FolderCategory::all();
+        $categories = FolderCategory::all();
         $numberOf = 0;
         foreach ($categories as $values) { $numberOf++; }
-        $category = App\Models\FolderCategory::find(rand(1, $numberOf));
+        $category = FolderCategory::find(rand(1, $numberOf));
         $this->get(route('category.show', $category->id))
         ->assertStatus(200)
         ->assertJsonStructure([
@@ -32,7 +33,7 @@ class CategoryTest extends TestCase
 
     public function test_can_list_category() 
     {
-        $this->get(route('category'))
+        $this->get(route('category.index'))
         ->assertStatus(200)
         ->assertJsonStructure([
             '*' =>                 
@@ -46,10 +47,10 @@ class CategoryTest extends TestCase
     }
 
     public function test_can_update_category() {
-        $categories = App\Models\FolderCategory::all();
+        $categories = FolderCategory::all();
         $numberOf = 0;
         foreach ($categories as $values) { $numberOf++; }
-        $category = App\Models\FolderCategory::find(rand(1, $numberOf));
+        $category = FolderCategory::find(rand(1, $numberOf));
         $data = [
             'title' => 'Test Title',
             'description' => 'This is a description test',
