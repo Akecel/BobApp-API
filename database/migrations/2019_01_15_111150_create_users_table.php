15a623ffdaaba5c8c5070facc5b1acb89f0f9fd4
Axel Rayer <axel.rayer@gmail.com>
2019-04-01 19:18:48 +0200

    set nullable user migration

--- database/migrations/2019_01_15_111150_create_users_table.php
+++ database/migrations/2019_01_15_111150_create_users_table.php
@@ -10,11 +10,11 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('phone_number')->unique();
-			$table->string('email')->default('NULL');
-			$table->string('password', 60)->default('NULL');
-			$table->string('lastName')->default('NULL');
-			$table->string('firstName')->default('NULL');
-			$table->string('birthdate')->default('NULL');
+			$table->string('email')->nullable();
+			$table->string('password', 60)->nullable();
+			$table->string('lastName')->nullable();
+			$table->string('firstName')->nullable();
+			$table->string('birthdate')->nullable();
 			$table->boolean('admin')->default(0);
 			$table->rememberToken('rememberToken');
 			$table->timestamps();
